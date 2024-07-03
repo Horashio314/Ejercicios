4b6a575ccc087c1fb2260e637680474fc0ae0847
Horacio Orrillo <32722566+Horashio314@users.noreply.github.com>
2024-07-03 05:37:26 -0400

    Ejercicio 1 FizzBuzz

---
+++ Ejecicios/Ejecicio_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejecicio_1
+{
+    class Program
+    {
+        private static string ValidarNumero(int numero)
+        {
+            if (numero % 3 == 0 & numero % 5 == 0)
+                return "FizzBuzz";
+            if (numero % 3 == 0)
+                return "Fizz";
+            if (numero % 5 == 0)
+                return "Buzz";
+            return numero.ToString();
+        }
+
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(ValidarNumero(i));
+            }
+        }
+    }
+}
