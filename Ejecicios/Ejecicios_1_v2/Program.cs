eae0da2f63e38b49cd1e79594bbbde8f02de0787
Horacio Orrillo <32722566+Horashio314@users.noreply.github.com>
2024-07-03 05:38:58 -0400

    Ejercicio 1 v2

---
+++ Ejecicios/Ejecicios_1_v2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejecicio_1
+{
+    class Program
+    {
+        private static string ValidarNumero(int numero)
+        {
+            string num = "";
+
+            if (numero % 3 == 0)
+                num += "Fizz";
+            if (numero % 5 == 0)
+                num += "Buzz";
+            if (num == "")
+                num = numero.ToString();
+
+            return num;
+        }
+
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(ValidarNumero(i));
+            }
+        }
+    }
+}
